6b377e83f54937f7f495b03f4d6960734b63d801
yuan-fang-228 <fy0228@gmail.com>
2022-09-29 15:08:45 +0000

    debug and fix

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
   let amenityDict = {}
   $('input:checkbox').change(function () {
     if ($(this).is(':checked')) {
-      amenityDict[(this).attr('amenity_id')] = $(this).attr('amenity_name')
+      amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
     } else {
-      delete amenityDict[(this).attr('amenity_id')]
+      delete amenityDict[$(this).attr('data-id')];
     }
+    $('.amenities h4').text(Object.values(amenityDict).join(', '));
   });
-  $(.amenities h4).text(Object.values(amenityDict).join(', '));
 });
