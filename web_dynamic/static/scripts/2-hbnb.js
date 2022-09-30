8a396df5570393316f7e5e1a1849d8144672fbe7
root <4479@holbertonstudents.com>
2022-09-30 03:23:48 +0000

    task 3 js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
+  $.ajax({
+    url:'http://82583f63b2ad.baaeebfa.hbtn-cod.io:5001/api/v1/status',
+    type: 'get',
+    dataType: 'json'
+  })
+  .done((data) => {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+	$('div#api_status').removeClass('available');
+    };
+  });
+
   let amenityDict = {}
+
   $('input:checkbox').change(function () {
     if ($(this).is(':checked')) {
       amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
