3cf72915167b791ae59a2273b0142547439f3150
yuan-fang-228 <fy0228@gmail.com>
2022-09-30 13:06:11 +0000

    change hard code to location.hostname

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  const url='http://ad7a1eb0ffcb.0bc3c855.hbtn-cod.io:5001/api/v1/status'
+  const url='http://' + location.hostname + ':5001/api/v1/status/';
   $.get(url, function (data) {
   if (data.status === 'OK') {
     $('div#api_status').addClass('available');
