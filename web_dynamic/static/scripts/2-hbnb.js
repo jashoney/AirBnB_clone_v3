307e51a4f881ab34dfe14d43428e77ab886ecf29
jashoney <4479@holbertonschool.com>
2022-09-30 10:43:49 +1000

    task3 js

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  let amenityDict = {}
+  $('input:checkbox').change(function () {
+    if ($(this).is(':checked')) {
+      amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenityDict[$(this).attr('data-id')];
+    }
+    $('.amenities h4').text(Object.values(amenityDict).join(', '));
+  });
+});
